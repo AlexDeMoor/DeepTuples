467e88e84c631788bb8b8dfa8998d5213c57e1ad
ademoor <alexandre.thierry.de.moor@cern.ch>
2025-05-19 16:03:29 +0200

    Add crab examples

---
+++ crab/crab_emu1_2023D.py
@@ -0,0 +1,33 @@
+import os
+from CRABClient.UserUtilities import config
+
+config = config()
+
+config.General.requestName = 'emu1_muon_2023D'
+config.General.workArea = 'crab_projects'
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '../DeepNtuplizer/production/DeepNtuplizer_emu.py'
+config.JobType.allowUndistributedCMSSW = True
+config.JobType.maxMemoryMB = 2500
+config.JobType.numCores    = 1
+
+config.Debug.extraJDL = ['+CMS_ALLOW_OVERFLOW=False']
+
+config.Data.inputDataset = "/Muon1/Run2023D-22Sep2023_v1-v1/MINIAOD"
+config.Data.outLFNDirBase = "/store/group/phys_btag/ParticleEdge/Edge_dataset/Domain_data/emu/2023D1"
+config.Data.outputDatasetTag = "part_2024"
+config.Data.inputDBS = "global"
+config.Data.publishDBS = "phys03"
+config.Data.splitting = "LumiBased"
+config.Data.ignoreLocality = False
+config.Data.unitsPerJob = 50
+config.JobType.maxJobRuntimeMin = 2750
+config.Data.lumiMask = '../json/Cert_Collisions2023_366442_370790_Golden.json'
+
+config.Site.storageSite = "T2_CH_CERN"
+config.Site.whitelist = ["T2_*","T3_*"]
+config.Site.blacklist = ["T2_BR_UERJ"]
+
+print("Done :)")
